050357024c726d573e3704c70f2c1c561afa49ef
Min Lu <37051693+luminous66@users.noreply.github.com>
2018-09-04 15:43:54 -0700

    support all gumball machine
    support any of the three Models in the Problem Domain.

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -3,44 +3,55 @@ public class GumballMachine
 {
 
     private int num_gumballs;
+    private int cost;
     private int enough_money;
+    private boolean nonquarter_accepted;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size ,int cost , boolean nonquarter_accepted)
     {
         // initialise instance variables
         this.num_gumballs = size;
+        this.cost = cost ;
         this.enough_money = 0;
+        this.nonquarter_accepted = nonquarter_accepted;
     }
 
     public void insertQuarter(int coin)
     {
-        if (enough_money<50) {
+        if (enough_money<this.cost) {
+            if(this.nonquarter_accepted)
             this.enough_money += coin ;
+            else{
+                if(coin==25)
+                this.enough_money += coin ;
+                else
+                System.out.println( "Only accept quarter." ) ;
+            }
         }
     }
     
     public void turnCrank()
     {
-    	if ( this.enough_money>=50 )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.enough_money = 0 ;
-    			System.out.println( "Thanks for your quarters.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarters." ) ;
-    		}
-    	}
-    	else if( ( this.enough_money>0 )&&( this.enough_money<50 ) )
-    	{
-    		System.out.println( this.enough_money+" cents received. Please insert more coins." ) ;
-    	} 
-    	else if( this.enough_money==0 )
-    	{
-    		System.out.println( "Please insert 50 cents." ) ;
-    	}
+        if ( this.enough_money>=this.cost )
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                this.enough_money = 0 ;
+                System.out.println( "Thanks for your quarters.  Gumball Ejected!" ) ;
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your quarters." ) ;
+            }
+        }
+        else if( ( this.enough_money>0 )&&( this.enough_money<this.cost ) )
+        {
+            System.out.println( this.enough_money+" cents received. Please insert more coins." ) ;
+        } 
+        else if( this.enough_money==0 )
+        {
+            System.out.println( "Please insert "+this.cost+" cents." ) ;
+        }
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -3,59 +3,104 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		//Model1:cost 25cents, accept only quarter
+	        GumballMachine gumballMachine1 = new GumballMachine(5,25,false);
 
-		System.out.println(gumballMachine);
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 0 );
-		gumballMachine.turnCrank();
+		System.out.println(gumballMachine1);
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 10 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		System.out.println(gumballMachine1);
+		
+		//Model2:cost 50cents, accept only quarter
+		GumballMachine gumballMachine2 = new GumballMachine(5,50,false);
 
-		System.out.println(gumballMachine);
+		System.out.println(gumballMachine2);
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 10 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		System.out.println(gumballMachine2);
+		
+		//Model3:cost 50cents, accept all coins
+		GumballMachine gumballMachine3 = new GumballMachine(5,50,true);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 10 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 5 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 10 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 5 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 10 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 10 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 5 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 10 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 10 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		System.out.println(gumballMachine);
+		System.out.println(gumballMachine3);
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 10 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 5 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 10 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 5 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 10 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 10 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 5 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 10 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 10 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		gumballMachine3.insertQuarter( 25 );
+		gumballMachine3.turnCrank();
+		
+		System.out.println(gumballMachine3);
 	}
 }
